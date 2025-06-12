a98b25f2c6337de3b60be65e93b27e369e943b68
labsis <sepreciado02@ucatolica.edu.co>
2025-06-12 04:07:55 -0500

    creacion de models.py

---
+++ models/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import date
+
+class Usuario(BaseModel):
+id: int
+nombre: str
+cc: int
+edad: int
+descripcion: Optional[str] = None
+imagen: Optional[str] = None
+# Información de la mascota principal del usuario
+nombre_mascota: str
+edad_mascota: int
+especie_mascota: Optional[str] = None
+peso_mascota: Optional[float] = None
+
+class Vuelo(BaseModel):
+localizador: str
+origen: str
+destino: str
+fecha: date
+sillas_res: int
+sillas_ven: int
